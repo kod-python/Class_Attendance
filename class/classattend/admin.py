dff900c4b8a332c1b8c8a8cdf7b7ef7f1c4749e9
kod-python <kelvinlik0245@gmail.com>
2024-07-15 16:09:03 +0000

    data added

--- class/classattend/admin.py
+++ class/classattend/admin.py
@@ -3,4 +3,18 @@ from .models import Attendance
 
 # Register your models here.
 
+class AttendanceInline(admin.TabularInline):
+     model = Attendance
+     raw_id_fields = ['attendances']
+     
+     
+     
+class AttendanceAdmin(admin.ModelAdmin):
+     list_display = ['id','firstName', 'lastName', 'email' 'course']
+      
+     list_filter = [ 'updated', 'created']
+     inlines = [AttendanceInline]
+
+
+
 admin.site.register(Attendance)
--- class/classattend/models.py
+++ class/classattend/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-    email= models.EmailField(max_length=254)
-    course = models.CharField(max_length=200)
+    firstName = models.CharField(max_length=100, default="firstName")
+    lastName = models.CharField(max_length=100, default="lastName")
+    email= models.EmailField(max_length=254, default="email")
+    course = models.CharField(max_length=200, default="course")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
